f2d14f797fed8bd315af0549acb8aae40164514e
Zach Wolf <hello@zachwolf.com>
2014-01-27 16:27:37 -0500

    Removing sample tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,19 +32,12 @@ module.exports = function(grunt) {
     css_stats: {
       default_options: {
         options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
+        }
       },
       custom_options: {
         options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
+          path: "test"
+        }
       },
     },
 
